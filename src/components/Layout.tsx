c3f28ab183200b642631c30cd8ac2767429e9202
Yannick <yannickkamdemkouam@yahoo.fr>
2025-11-14 20:35:06 +0100

    Hiding navigation on login

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,22 @@
-
 import { ReactNode } from 'react';
 import Navigation from './Navigation';
+import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+
+  // Liste des routes où la navigation doit être masquée
+  const hideNavRoutes = ["/"];
+
+  const shouldHideNav = hideNavRoutes.includes(location.pathname);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Navigation />
+      {!shouldHideNav && <Navigation />}  {/* ← caché si on est sur / */}
       <main className="flex-1">
         {children}
       </main>
